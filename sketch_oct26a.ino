0a298327387f1121ba453461f18959caea2f725c
evenbrownie <evenbrownie@gmail.com>
2017-11-02 13:54:09 -0700

    Added P to error control, set up led light system and added code to implement a slow stop upon coming up to the line

--- sketch_oct26a.ino
+++ sketch_oct26a.ino
@@ -3,15 +3,15 @@
 #define midSensor 15
 #define leftSensor 16
 
-// HBridgeL          //dont use these unless with an hbridge
-//#define LAnalog 10
-//#define LIn1 7
-//#define LIn2 8
+ //HBridgeL          //dont use these unless with an hbridge
+#define LAnalog 10
+#define LIn1 7
+#define LIn2 8
 
 // HBridgeR     //dont use these unless with an hbridge
-//#define RAnalog 9
-//#define RIn3 5
-//#define RIn4 6
+#define RAnalog 9
+#define RIn3 5
+#define RIn4 6
 
 //LEDs
 #define ledRed 12 
@@ -25,18 +25,23 @@ int vRightSensor;
 int vMidSensor;
 
 // Left Motor
-int L1 = HIGH;   //defines motion as forward
-int L2 = LOW;
+int L1;    //defines motion as forward
+int L2;
 int maxSpeedL = 252;     //adjust the speed of the wheels to match eatch other
 int midSpeedL;
-int SpeedL;
+int speedL;
+int straightL = 93;
 
 // Right Motor
-int R1 = HIGH;    //defines motion as forward
-int R2 = LOW;
+int R1;   //defines motion as forward
+int R2;
 int maxSpeedR = 255;  //adjust the speed of the wheels to match each other
 int midSpeedR;
-int SpeedR;
+int speedR;
+int straightR = 100;
+
+int motorState1= HIGH;
+int motorState0 = LOW;
 
 //Time Variables
 int currentMillis;
@@ -50,7 +55,7 @@ int zeroL = 0;
 int zeroR = 0;
 
 //PID Variables
-float p;
+float p = -.03;
 float i;
 float d; 
 
@@ -67,6 +72,15 @@ pinMode(leftSensor, INPUT);
 pinMode (ledRed, OUTPUT);
 pinMode (ledGreen, OUTPUT);
 pinMode (ledBlue, OUTPUT);
+// Left Motor
+  pinMode(LAnalog, OUTPUT);
+  pinMode(LIn1, OUTPUT);
+  pinMode(LIn2, OUTPUT);
+
+  // Right Motor
+  pinMode(RAnalog, OUTPUT);
+  pinMode(RIn3, OUTPUT);
+  pinMode(RIn4, OUTPUT);
 Serial.begin(9600);
 
 // zero
@@ -75,6 +89,7 @@ Serial.begin(9600);
     zeroM += analogRead(midSensor);
     zeroL += analogRead(leftSensor);
     zeroR += analogRead(rightSensor);
+    
     delay(7);
   }
   zeroM /= nSamples;
@@ -97,29 +112,38 @@ void loop() {
   vMidSensor= analogRead(midSensor);
   vLeftSensor = analogRead(leftSensor);
   }
-  digitalWrite(ledRed, HIGH);
-  digitalWrite(ledBlue, HIGH);
-  digitalWrite(ledGreen, HIGH);
-Serial.println(vRightSensor);
-  //if(maxSpeedL > maxSpeedR){
-    //digitalWrite(ledBlue, HIGH);
-  //}
 
-  //if(maxSpeedL < maxSpeedR){
-    //digitalWrite(ledRed, HIGH);
-  //}
+  if (abs(pErrorL) > 300 && abs(pErrorR) > 300){
+    motorState1 = LOW;
+  }
 
-  //if(
-  
+
+
+  pErrorL = zeroL - vLeftSensor;
+  pErrorR = zeroR - vRightSensor;
+
+  speedL = straightL - (p*pErrorL);
+  speedR = straightR - (p*pErrorR);
   
   //analogWrite to motors depending on sensor values
  // Left Motor
-  //analogWrite(LAnalog, maxSpeedL);
-  //digitalWrite(LIn1, HIGH);   //May need to adjust the high low depending on configuration
-  //digitalWrite(LIn2, LOW);
+  //analogWrite(LAnalog, speedL);      //93 for now
+ // digitalWrite(LIn1, motorState1);   //May need to adjust the high low depending on configuration
+  //digitalWrite(LIn2, motorState0);
 
   // Right Motor
-  //analogWrite(RAnalog, maxSpeedR);
-  //digitalWrite(RIn3, HIGH);   //May need to adjust the high low depending on configuration
-  //digitalWrite(RIn4, LOW);
+  //analogWrite(RAnalog, speedR);   //100 for now
+  //digitalWrite(RIn3, motorState1);   //May need to adjust the high low depending on configuration
+  //digitalWrite(RIn4, motorState0);
+
+  if((speedL+7) > speedR){
+    digitalWrite(ledBlue, HIGH);
+  }
+
+  if((speedL+7) < speedR){
+    digitalWrite(ledRed, HIGH);
+  }
+  if((speedL+7 == speedR){
+    digitalWrite(ledGreen, HIGH);
+  }
 }
